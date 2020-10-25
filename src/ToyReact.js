651eaf938d66081d987566dd51e3608afb5bef3d
ByeWord <694745028@qq.com>
2020-10-25 21:13:01 +0800

    添加reRender和setState逻辑

--- src/ToyReact.js
+++ src/ToyReact.js
@@ -1,18 +1,42 @@
+const RENDER_TO_DOM = Symbol("render to dom");
+
 class ElementWrapper {
-    constructor(tag) {
-        this.root = document.createElement(tag);
+    constructor(type) {
+        this.root = document.createElement(type);
     }
 
     setAttribute(name, value) {
-        this.root.setAttribute(name, value);
+        if (name.match(/^on([\s\S]+)/)) {
+            this.root.addEventListener(RegExp.$1.replace(/^[\s\S]/, c => c.toLowerCase()), value)
+        } else {
+            if (name === "className") {
+                this.root.setAttribute("class", value);
+            } else {
+                this.root.setAttribute(name, value);
+            }
+        }
     }
 
-    /***
+    /**
      *
-     * @param component {Component | ElementWrapper}
+     * @param component {ElementWrapper | TextWrapper | Component}
      */
     appendChild(component) {
-        this.root.appendChild(component.root);
+        const range = document.createRange();
+        range.setStart(this.root, this.root.childNodes.length);
+        range.setEnd(this.root, this.root.childNodes.length);
+        // range.deleteContents();
+        // this.root.appendChild(component.root);
+        component[RENDER_TO_DOM](range);
+    }
+
+    /**
+     *
+     * @param range {Range}
+     */
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
     }
 }
 
@@ -20,74 +44,145 @@ class TextWrapper {
     constructor(content) {
         this.root = document.createTextNode(content);
     }
+
+    /**
+     * let oldRange = this._range;
+     const range = document.createRange();
+     range.setStart(this._range.startContainer, this._range.startOffset);
+     range.setEnd(this._range.startContainer, this._range.startOffset);
+     this[RENDER_TO_DOM](range);
+     oldRange.setStart(oldRange.endContainer, range.endOffset);
+     oldRange.deleteContents();
+     * @param range
+     */
+    [RENDER_TO_DOM](range) {
+        range.deleteContents();
+        range.insertNode(this.root);
+    }
 }
 
 export class Component {
     constructor() {
         this.props = Object.create(null);
+        /**@type {Array<Component|TextWrapper|ElementWrapper>}*/
         this.children = [];
         this._root = null;
+        this._range = null;
     }
 
     setAttribute(name, value) {
         this.props[name] = value;
     }
 
+    /**
+     *
+     * @param component {ElementWrapper | TextWrapper | Component}
+     */
     appendChild(component) {
-        this.children.push(component);
+        this.children.push(component)
     }
 
-    get root() {
-        if (this._root === null) {
-            this._root = this.render().root;
-        }
-        return this._root;
+    // get root() {
+    //     if (!this._root) {
+    //         this._root = this.render().root;
+    //     }
+    //     return this._root;
+    // }
+
+    /**
+     *
+     * @param range {Range}
+     */
+    [RENDER_TO_DOM](range) {
+        this._range = range;
+        const renderResult = this.render();
+        return renderResult[RENDER_TO_DOM](range);
     }
 
     render() {
-        throw new Error();
+        throw new Error()
+    }
+
+    reRender() {
+        this._range.deleteContents();
+        this[RENDER_TO_DOM](this._range);
+    }
+
+    setState(newState) {
+        if (
+            this.state === null
+            || typeof this.state !== "object"
+        ) {
+            this.state = newState;
+            this.reRender();
+            return;
+        }
+        const merge = (oldState, _newState) => {
+            for (let p in _newState) {
+                if (oldState[p] === null
+                    || typeof oldState[p] !== "object") {
+                    oldState[p] = _newState[p];
+                } else {
+                    merge(oldState[p], _newState[p]);
+                }
+            }
+        }
+        merge(this.state, newState);
+        console.log(this.state);
+        this.reRender();
     }
 }
 
 /**
  *
- * @param tag
- * @param props {Object}
- * @param children
- * @return {ElementWrapper}
+ * @param type {string | Function}
+ * @param props {null|Object}
+ * @param children {ElementWrapper|TextWrapper|Component|Array|string}
+ * @returns {ElementWrapper | Component}
  */
-export function createElement(tag, props, ...children) {
-    console.log(children)
-    let e;
-    if (typeof tag === "string") {
-        e = new ElementWrapper(tag);
+export function createElement(type, props, ...children) {
+    let dom;
+    if (typeof type === "function") {
+        dom = new type();
     } else {
-        e = new tag();
+        dom = new ElementWrapper(type);
     }
     for (const prop in props) {
-        if (Object.prototype.hasOwnProperty.call(props, prop)) {
-            e.setAttribute(prop, props[prop]);
-        }
+        dom.setAttribute(prop, props[prop])
     }
+    /**
+     *
+     * @param children {Array<ElementWrapper|TextWrapper|Component|Array|string>}
+     */
     let insertChildren = (children) => {
         for (const child of children) {
-            let childEl;
-            if (typeof child === "string") {
-                childEl = new TextWrapper(child)
-            }else {
-                childEl = child;
-            }
-            if (typeof child === "object" && child instanceof Array) {
+            let el;
+            if (typeof child === "string" || typeof child === "number") {
+                el = new TextWrapper(child + "");
+            } else if (Array.isArray(child)) {
                 insertChildren(child);
-            } else {
-                e.appendChild(childEl);
+            } else if (typeof child === "object") {
+                el = child;
+            } else if (child === null) {
+                return;
             }
+            if (el) dom.appendChild(el);
         }
     }
     insertChildren(children);
-    return e;
+    return dom;
 }
 
-export function render(component, parentEl) {
-    parentEl.appendChild(component.root);
+
+/**
+ *
+ * @param component {TextWrapper|ElementWrapper|Component}
+ * @param parentElement {HTMLElement}
+ */
+export function render(component, parentElement) {
+    const range = document.createRange();
+    range.setStart(parentElement, 0);
+    range.setEnd(parentElement, parentElement.childNodes.length);
+    range.deleteContents();
+    component[RENDER_TO_DOM](range);
 }
--- src/main.js
+++ src/main.js
@@ -1,22 +1,218 @@
-import {createElement, Component, render} from "./ToyReact";
+import {Component, createElement, render} from "./toy-react";
 
-class MyComponent extends Component {
+class Square extends Component {
+    constructor() {
+        super();
+    }
+
     render() {
-        return <div>
-            {this.children}
-        </div>
+        return (
+            <button className="square" onClick={this.props.onClick}>
+                {this.props.value}
+            </button>
+        );
     }
 }
 
+class Board extends Component {
+    renderSquare(i) {
+        return (
+            <Square
+                value={this.props.squares[i]}
+                onClick={() => this.props.onClick(i)}
+            />
+        );
+    }
 
-const a = <MyComponent>
-    <div id={1}>1</div>
-    <div id={2}>2</div>
-    <div id={3}>3</div>
-    <div id={4}>
-        <div id={"4-1"}>4</div>
-    </div>
-</MyComponent>
+    render() {
+        return (
+            <div>
+                <div className="board-row">
+                    {this.renderSquare(0)}
+                    {this.renderSquare(1)}
+                    {this.renderSquare(2)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(3)}
+                    {this.renderSquare(4)}
+                    {this.renderSquare(5)}
+                </div>
+                <div className="board-row">
+                    {this.renderSquare(6)}
+                    {this.renderSquare(7)}
+                    {this.renderSquare(8)}
+                </div>
+            </div>
+        );
+    }
+}
 
-render(a,document.getElementById("app"))
+class Game extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            history: [
+                {
+                    squares: Array(9).fill(null)
+                }
+            ],
+            stepNumber: 0,
+            xIsNext: true
+        };
+    }
 
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber + 1);
+        const current = history[history.length - 1];
+        const squares = current.squares.slice();
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? "X" : "O";
+        this.setState({
+            history: history.concat([
+                {
+                    squares: squares
+                }
+            ]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext
+        });
+    }
+
+    jumpTo(step) {
+        this.setState({
+            stepNumber: step,
+            xIsNext: (step % 2) === 0
+        });
+    }
+
+    render() {
+        const history = this.state.history;
+        const current = history[this.state.stepNumber];
+        const winner = calculateWinner(current.squares);
+
+        const moves = history.map((step, move) => {
+            const desc = move ?
+                'Go to move #' + move :
+                'Go to game start';
+            return (
+                <li key={move}>
+                    <button onClick={() => this.jumpTo(move)}>{desc}</button>
+                </li>
+            );
+        });
+
+        let status;
+        if (winner) {
+            status = "Winner: " + winner;
+        } else {
+            status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+        }
+
+        return (
+            <div className="game">
+                <div className="game-board">
+                    <Board
+                        squares={current.squares}
+                        onClick={i => this.handleClick(i)}
+                    />
+                </div>
+                <div className="game-info">
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
+                </div>
+            </div>
+        );
+    }
+}
+
+// ========================================
+
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6]
+    ];
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i];
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a];
+        }
+    }
+    return null;
+}
+
+render(<Game/>, document.getElementById("app"));
+
+// class MyComponent extends Component {
+//     constructor() {
+//         super();
+//         this.state = {
+//             count: 0
+//         }
+//     }
+//
+//     doAdd() {
+//         this.setState({
+//             count: this.state.count + 1
+//         })
+//     }
+//
+//     doDesc() {
+//         this.setState({
+//             count: this.state.count - 1
+//         })
+//     }
+//
+//     render() {
+//         const {count} = this.state;
+//         return <div id={this.props.id}>
+//             <p>{count}</p>
+//             <div>
+//                 <button onClick={() => this.doAdd()}>+</button>
+//                 <button onClick={() => this.doDesc()}>-</button>
+//             </div>
+//         </div>
+//     }
+// }
+//
+// // const a = <div>
+// //     <div>apple</div>
+// // </div>
+// // console.log(a.root)
+// // document.body.appendChild(a.root)
+// // createElement("div",null,[
+// //     createElement("div",null,[
+// //         "a",
+// //         createElement("div",null,["b"])
+// //     ]),
+// //     createElement(MyComponent,null,[])
+// // ])
+// // const a = <MyComponent>
+// //     <div>1</div>
+// //     <div>2</div>
+// // </MyComponent>
+//
+//
+// const c = createElement(
+//     MyComponent,
+//     {id: "my-component"},
+//     createElement("div", null, "1"),
+//     createElement("div", null, "2")
+// )
+// // console.log(c)
+// // const b = (<div>123</div>)
+// // console.log(b)
+// // console.log(b.constructor)
+// // console.log(a)
+// // console.log(a.constructor)
+// // console.log(a.root)
+//
+// render(c, document.getElementById("app"))
